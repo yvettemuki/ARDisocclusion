eb6016aebe263b268d65a5619c6b1d58af523be6
yvettemuki <373628977@qq.com>
2022-06-08 18:02:33 -0400

    feat: change the human direction dataset

--- Assets/Scripts/UserStudyAPIs.cs
+++ Assets/Scripts/UserStudyAPIs.cs
@@ -196,13 +196,34 @@ public class UserStudyAPIs : MonoBehaviour
     public string GetDirectIndicateAccuracy()
     {
         string dataset = null;
-        Vector3 correct_dir = (m_UserStudyController.GetCurrHumanPos() - m_ARController.m_ARCamera.transform.position).normalized;
-        Vector3 user_dir = m_ARController.m_ARCamera.transform.forward;
-        float deviation_angle = Mathf.Abs(Vector3.Angle(correct_dir, user_dir));
+        //Vector3 correct_dir = (m_UserStudyController.GetCurrHumanPos() - m_ARController.m_ARCamera.transform.position).normalized;
+        //Vector3 user_dir = m_ARController.m_ARCamera.transform.forward;
+        //float deviation_angle = Mathf.Abs(Vector3.Angle(correct_dir, user_dir));
         //float diff_angle = (90f - Mathf.Abs(Vector3.Angle(correct_dir, user_dir))) / 90f * 100f;
-        dataset = $"{correct_dir.x.ToString("0.00")}#{correct_dir.y.ToString("0.00")}#{correct_dir.z.ToString("0.00")}," +
-            $"{user_dir.x.ToString("0.00")}#{user_dir.y.ToString("0.00")}#{user_dir.z.ToString("0.00")}," +
-            $"{deviation_angle.ToString("0.00")}";
+        //dataset = $"{correct_dir.x.ToString("0.00")}#{correct_dir.y.ToString("0.00")}#{correct_dir.z.ToString("0.00")}," +
+        //    $"{user_dir.x.ToString("0.00")}#{user_dir.y.ToString("0.00")}#{user_dir.z.ToString("0.00")}," +
+        //    $"{deviation_angle.ToString("0.00")}";
+
+        // User position with respect to the portal
+        Vector3 user_position_in_portal = Vector3.zero;
+        Vector3 user_position_in_world = m_ARController.m_ARCamera.transform.position;
+        m_ARController.WorldObjectPos2Portal(in user_position_in_world, out user_position_in_portal);
+
+        // User direction with respect to the portal
+        Quaternion user_rotation_in_portal = Quaternion.identity;
+        Transform user_transform_in_world = m_ARController.m_ARCamera.transform;
+        m_ARController.WorldObjectRot2Portal(in user_transform_in_world, out user_rotation_in_portal);
+        Vector3 user_direction_in_portal = user_rotation_in_portal * Vector3.forward;
+
+        // Human sprite depth(z) with respect to the portal
+        Vector3 human_sprite_position_in_portal = Vector3.zero;
+        Vector3 human_sprite_position_in_world = m_UserStudyController.GetCurrHumanPos();
+        m_ARController.WorldObjectPos2Portal(in human_sprite_position_in_world, out human_sprite_position_in_portal);
+        float human_sprite_depth_in_portal = human_sprite_position_in_portal.z;
+
+        dataset = $"{user_position_in_portal.x.ToString("0.000")}#{user_position_in_portal.y.ToString("0.000")}#{user_position_in_portal.z.ToString("0.000")}," +
+            $"{user_direction_in_portal.x.ToString("0.000")}#{user_direction_in_portal.y.ToString("0.000")}#{user_direction_in_portal.z.ToString("0.000")}," +
+            $"{human_sprite_depth_in_portal.ToString("0.000")}";
         Debug.Log(dataset);
         
         return dataset;
